dcb2452a9837e797713d147c71770b45e642aeca
Ivan Gonzalez <ivan@simplelogica.net>
2016-06-23 11:00:45 +0200

    new version jueves, 23 de junio de 2016, 11:00:45 CEST

---
+++ javascripts/all.js
@@ -0,0 +1,1 @@
+$(function(){$.each(["white","yellow","orange","red","magenta","purple","blue","cyan","green","darkgreen","brown","tan","lightgrey","#808080","darkgrey","black"],function(){$("#tools #colors").append("<a href='#' class='color' data-color='"+this+"' style='background: "+this+";'></a> ")})});
